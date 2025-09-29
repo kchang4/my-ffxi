627548e6cffb805614802939b9b4c8185e5a6730
WinterSolstice8 <60417494+wintersolstice8@users.noreply.github.com>
2025-09-28 19:30:13 -0600

    [login] Handle hung session edge case, remove session if verifably stale

--- src/login/data_session.cpp
+++ src/login/data_session.cpp
@@ -310,6 +310,17 @@ void data_session::read_func()
                 ShowInfo(fmt::format("data_session: zoneid: {}, zoneipp: {}:{}, searchipp: {}:{}, for charid: {}",
                                      ZoneID, ip2str(ZoneIP), ZonePort, ip2str(characterSelectionResponse.cache_ip), characterSelectionResponse.cache_port, charid));
 
+                // If client was zoning out but was never seen at the destination past 2 minutes, remove old session
+                const auto rset2 = db::preparedStmt("SELECT * "
+                                                    "FROM accounts_sessions "
+                                                    "WHERE accid = ? AND charid = ? AND client_port = '0' AND last_zoneout_time <= SUBTIME(NOW(), \"00:02:00\")",
+                                                    session.accountID, charid);
+                if (rset2 && rset2->rowsCount() != 0 && rset2->next())
+                {
+                    // KillSession? Seems overkill with current knowledge. client_port of 0 indicates the other map server never saw a packet and decrypted it correctly.
+                    db::preparedStmt("DELETE FROM accounts_sessions WHERE accid = ? AND charid = ?", session.accountID, charid);
+                }
+
                 // Check the number of sessions
                 uint16 sessionCount = 0;
 
