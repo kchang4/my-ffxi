4fe5433e2148f2e3d78d0884e2581c26d1f43eb2
MowFord <131182600+MowFord@users.noreply.github.com>
2025-10-08 11:11:01 -0500

    Mob pet definitions to lua: zones 105-126

--- scripts/zones/Meriphataud_Mountains/mobs/Yagudo_Mendicant.lua
+++ scripts/zones/Meriphataud_Mountains/mobs/Yagudo_Mendicant.lua
@@ -1,16 +1,12 @@
 -----------------------------------
--- Area: Sauromugue Champaign
---  Mob: Yagudo Oracle
+-- Area: Meriphataud Mountains
+--   NM: Yagudo Mendicant
 -----------------------------------
 ---@type TMobEntity
 local entity = {}
 
 entity.onMobInitialize = function(mob)
     xi.pet.setMobPet(mob, 1, 'Yagudos_Elemental')
-end
-
-entity.onMobDeath = function(mob, player, optParams)
-    xi.regime.checkRegime(player, mob, 99, 2, xi.regime.type.FIELDS)
 end
 
 return entity
