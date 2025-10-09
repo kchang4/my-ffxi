9730f12070d71e7f084d73f631f80fad5f9c0248
MowFord <131182600+MowFord@users.noreply.github.com>
2025-10-09 14:14:19 -0500

    Mob pet definitions to lua: zones 153-159

--- scripts/zones/Castle_Zvahl_Keep_[S]/mobs/Demon_Condemner.lua
+++ scripts/zones/Castle_Zvahl_Keep_[S]/mobs/Demon_Condemner.lua
@@ -1,15 +1,12 @@
 -----------------------------------
--- Area: Upper Delkfutt's Tower
---   NM: Pallas
------------------------------------
-mixins = { require('scripts/mixins/job_special') }
+-- Area: Castle Zvahl Keep [S]
+--  Mob: Demon Condemner
 -----------------------------------
 ---@type TMobEntity
 local entity = {}
 
 entity.onMobInitialize = function(mob)
-    xi.pet.setMobPet(mob, 1, 'Gigass_Bat')
-    mob:setMobMod(xi.mobMod.IDLE_DESPAWN, 120)
+    xi.pet.setMobPet(mob, 1, 'Demons_Elemental')
 end
 
 entity.onMobDeath = function(mob, player, optParams)
