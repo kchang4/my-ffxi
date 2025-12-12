a69578463c79fd0f3722d240f734ef93abf8f7c9
Xaver-DaRed <xaver.entropia@gmail.com>
2025-12-12 07:02:46 +0100

    Create a summon setup global file

--- scripts/zones/Sacrificial_Chamber/mobs/Tonberrys_Elemental.lua
+++ scripts/zones/Sacrificial_Chamber/mobs/Tonberrys_Elemental.lua
@@ -2,18 +2,25 @@
 -- Area: Sacrificial Chamber
 -- Name: Tonberrys Elemental
 -----------------------------------
-mixins = { require('scripts/mixins/families/elemental_pet') }
+mixins = { require('scripts/mixins/pet_summon_setup') }
 -----------------------------------
 ---@type TMobEntity
 local entity = {}
 
 entity.onMobInitialize = function(mob)
-    xi.elemental_pet.mobElements[mob:getID()] =
+    local possibleSummons =
     {
-        xi.elemental_pet.type.WATER,
-        xi.elemental_pet.type.LIGHT,
-        xi.elemental_pet.type.FIRE,
+        xi.pets.summon.type.FIRE_SPIRIT,
+        xi.pets.summon.type.WATER_SPIRIT,
+        xi.pets.summon.type.LIGHT_SPIRIT,
     }
+
+    local summonBitmask = 0
+    for _, entry in ipairs(possibleSummons) do
+        utils.mask.setBit(summonBitmask, entry, true)
+    end
+
+    mob:setLocalVar('[Summon]mask', summonBitmask)
 end
 
 return entity
