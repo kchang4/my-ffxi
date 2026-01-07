8b6d0c31b6dd781448c8d3a3cf124c57c11701d7
Xaver-DaRed <xaver.entropia@gmail.com>
2026-01-07 18:51:59 +0100

    Change "Velocity Shot" to be a "Main job only" ability

--- sql/abilities.sql
+++ sql/abilities.sql
@@ -240,7 +240,7 @@ INSERT INTO `abilities` VALUES (220,'alacrity',20,25,1,1,231,0,0,178,2000,0,6,0,
 INSERT INTO `abilities` VALUES (221,'ebullience',20,55,1,1,231,0,0,179,2000,0,6,0,0,0,1,80,0,32,'WOTG');
 INSERT INTO `abilities` VALUES (222,'manifestation',20,40,1,1,231,0,0,180,2000,0,6,0,0,0,1,80,0,32,'WOTG');
 INSERT INTO `abilities` VALUES (223,'stratagems',20,10,1,0,233,0,0,0,2000,0,6,0,0,0,0,0,0,0,'WOTG');
-INSERT INTO `abilities` VALUES (224,'velocity_shot',11,45,1,60,129,0,0,186,2000,0,6,0,0,0,1,300,0,0,'WOTG');
+INSERT INTO `abilities` VALUES (224,'velocity_shot',11,45,1,60,129,0,0,186,2000,0,6,0,0,0,1,300,0,4,'WOTG');
 INSERT INTO `abilities` VALUES (225,'snarl',9,45,257,30,107,528,0,87,2000,0,6,10,0,0,0,0,0,192,'WOTG');
 INSERT INTO `abilities` VALUES (226,'retaliation',1,60,1,180,8,0,0,185,2000,0,6,0,0,0,1,80,0,0,'WOTG');
 INSERT INTO `abilities` VALUES (227,'footwork',2,65,1,300,21,0,0,197,2000,0,6,0,0,0,1,80,0,0,'WOTG');
