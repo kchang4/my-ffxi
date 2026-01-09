5df5345fb1cf541089718067c99a89efeedc09f8
Xaver-DaRed <xaver.entropia@gmail.com>
2026-01-09 20:08:50 +0100

    Reorder TP functions by "local/global" usage

--- scripts/globals/combat/tp.lua
+++ scripts/globals/combat/tp.lua
@@ -3,6 +3,98 @@ xi.combat = xi.combat or {}
 xi.combat.tp = xi.combat.tp or {}
 -----------------------------------
 
+-----------------------------------
+-- "Local" functions only used here.
+-----------------------------------
+
+-- https://www.bg-wiki.com/ffxi/Tactical_Points
+-- Gainee is the target who is going to gain the TP.
+-- For instance, if a player attacks a mob, the mob uses the mob formula when gaining TP from the returned hit.
+-- This appears to be a measure to not buff mobs when players were buffed with the new TP gain formula.
+xi.combat.tp.calculateTPReturn = function(gainee, delay)
+    local tpReturn = 0
+
+    if gainee and gainee:getObjType() ~= xi.objType.MOB then -- Pets and PCs have been observed to use this formula
+        if delay > 900 then
+            tpReturn = 173 + (delay - 900) * 28 / 360
+        elseif delay > 720 then
+            tpReturn = 161 + (delay - 720) * 24 / 360
+        elseif delay > 630 then
+            tpReturn = 154 + (delay - 630) * 28 / 360
+        elseif delay > 540 then
+            tpReturn = 149 + (delay - 540) * 20 / 360
+        elseif delay > 180 then
+            tpReturn = 61 + (delay - 180) * 88 / 360
+        else
+            tpReturn = 61 + (delay - 180) * 63 / 360
+        end
+    else -- mobs have been observed to use this formula -- http://wiki.ffo.jp/html/308.html
+        if delay > 530 then
+            tpReturn = 145 + (delay - 530) * 35 / 470
+        elseif delay > 480 then
+            tpReturn = 130 + (delay - 480) * 15 / 30
+        elseif delay > 450 then
+            tpReturn = 115 + (delay - 450) * 15 / 30
+        elseif delay > 180 then
+            tpReturn = 50 + (delay - 180) * 65 / 270
+        else
+            tpReturn = 50 + (delay - 180) * 15 / 180
+        end
+    end
+
+    return math.floor(tpReturn)
+end
+
+xi.combat.tp.getModifiedDelayAndCanZanshin = function(actor, delay)
+    local modifiedDelay = delay
+    local canZanshin    = false
+
+    -- DW/H2H delay is halved for the purposes of a single hit's TP return when applicable, see https://www.bg-wiki.com/ffxi/Tactical_Points
+    if actor:isDualWielding() then -- NOTE: this 'isDualWielding' may trip on non-PCs even if they are 'using h2h'. If this is rectified in core in the future this should fall through correctly.
+        modifiedDelay = (delay * (100 - actor:getMod(xi.mod.DUAL_WIELD)) / 100) / 2
+    elseif actor:isUsingH2H() then
+        if actor:getObjType() == xi.objType.PC then            -- handle h2h with > 1 swing only on PC
+            if
+                actor:getEquippedItem(xi.slot.SUB) ~= nil or   -- equipped shield = one swing
+                actor:getSkillRank(xi.skill.HAND_TO_HAND) == 0 -- zero h2h rank skill = one swing
+            then
+                modifiedDelay = math.max((delay - actor:getMod(xi.mod.MARTIAL_ARTS)), 96) -- min delay of 96 total, https://www.bg-wiki.com/ffxi/Attack_Speed
+                canZanshin    = true -- Zanshin can proc on an 'unarmed' swing               -- https://www.bg-wiki.com/ffxi/Zanshin
+            else
+                modifiedDelay = math.max((delay - actor:getMod(xi.mod.MARTIAL_ARTS)) / 2, 48) -- min delay of 96 total so 96/2 per fist, https://www.bg-wiki.com/ffxi/Attack_Speed
+            end
+        else
+            -- TODO: handle the corner case where a PC-like entity is using h2h but is only hitting with one 'fist'. Perhaps they have a shield with no main weapon.
+            -- elseif actor:getAutoAttackHits() > 1
+            modifiedDelay = math.max((delay - actor:getMod(xi.mod.MARTIAL_ARTS)) / 2, 48)
+        end
+    else -- single melee swing, either 1H or 2H
+        canZanshin = true -- https://www.bg-wiki.com/ffxi/Zanshin
+    end
+
+    modifiedDelay = modifiedDelay * math.max((100 + actor:getMod(xi.mod.DELAYP)) / 100, 0.85) -- minimum cap of -15% https://www.bg-wiki.com/ffxi/Attack_Speed. Undocumented if 15% + Claymore Grip goes above 15%.
+
+    return ({ canZanshin = canZanshin , modifiedDelay = math.floor(modifiedDelay) })
+end
+
+-- Bonus subtle blow II from Tandem Blow (BST trait)
+xi.combat.tp.getTandemBlowBonus = function(actor)
+    local tandemBlowBonus = 0
+    if actor:isTandemActive() then
+        if actor:getMaster() ~= nil and actor:getMaster():isPC() then
+            tandemBlowBonus = actor:getMaster():getMod(xi.mod.TANDEM_BLOW_POWER)
+        else
+            tandemBlowBonus = actor:getMod(xi.mod.TANDEM_BLOW_POWER)
+        end
+    end
+
+    return tandemBlowBonus
+end
+
+-----------------------------------
+-- Global functions used elsewhere.
+-----------------------------------
+
 -- Returns attacker TP gain from a single melee hit from itself.
 xi.combat.tp.getSingleMeleeHitTPReturn = function(actor, isZanshin)
     if actor:hasStatusEffect(xi.effect.MEIKYO_SHISUI) then
@@ -43,101 +135,21 @@ xi.combat.tp.getSingleWeaponTPReturn = function(actor, slot)
     return math.floor(tpReturn * storeTPModifier)
 end
 
-xi.combat.tp.getModifiedDelayAndCanZanshin = function(actor, delay)
-    local modifiedDelay = delay
-    local canZanshin    = false
-
-    -- DW/H2H delay is halved for the purposes of a single hit's TP return when applicable, see https://www.bg-wiki.com/ffxi/Tactical_Points
-    if actor:isDualWielding() then -- NOTE: this 'isDualWielding' may trip on non-PCs even if they are 'using h2h'. If this is rectified in core in the future this should fall through correctly.
-        modifiedDelay = (delay * (100 - actor:getMod(xi.mod.DUAL_WIELD)) / 100) / 2
-    elseif actor:isUsingH2H() then
-        if actor:getObjType() == xi.objType.PC then            -- handle h2h with > 1 swing only on PC
-            if
-                actor:getEquippedItem(xi.slot.SUB) ~= nil or   -- equipped shield = one swing
-                actor:getSkillRank(xi.skill.HAND_TO_HAND) == 0 -- zero h2h rank skill = one swing
-            then
-                modifiedDelay = math.max((delay - actor:getMod(xi.mod.MARTIAL_ARTS)), 96) -- min delay of 96 total, https://www.bg-wiki.com/ffxi/Attack_Speed
-                canZanshin    = true -- Zanshin can proc on an 'unarmed' swing               -- https://www.bg-wiki.com/ffxi/Zanshin
-            else
-                modifiedDelay = math.max((delay - actor:getMod(xi.mod.MARTIAL_ARTS)) / 2, 48) -- min delay of 96 total so 96/2 per fist, https://www.bg-wiki.com/ffxi/Attack_Speed
-            end
-        else
-            -- TODO: handle the corner case where a PC-like entity is using h2h but is only hitting with one 'fist'. Perhaps they have a shield with no main weapon.
-            -- elseif actor:getAutoAttackHits() > 1
-            modifiedDelay = math.max((delay - actor:getMod(xi.mod.MARTIAL_ARTS)) / 2, 48)
-        end
-    else -- single melee swing, either 1H or 2H
-        canZanshin = true -- https://www.bg-wiki.com/ffxi/Zanshin
+-- UNUSED.
+-- Returns a single ranged hit's TP return
+xi.combat.tp.getSingleRangedHitTPReturn = function(actor)
+    if actor:hasStatusEffect(xi.effect.MEIKYO_SHISUI) then
+        return 0
     end
 
-    modifiedDelay = modifiedDelay * math.max((100 + actor:getMod(xi.mod.DELAYP)) / 100, 0.85) -- minimum cap of -15% https://www.bg-wiki.com/ffxi/Attack_Speed. Undocumented if 15% + Claymore Grip goes above 15%.
-
-    return ({ canZanshin = canZanshin , modifiedDelay = math.floor(modifiedDelay) })
-end
-
--- returns a single melee hit's TP return
-xi.combat.tp.getSingleRangedHitTPReturn = function(actor, target)
     local delay = actor:getBaseRangedDelay() -- there do not appear to be any delay modifiers for ranged attacks, snapshot does not seem to effect this
-
-    if delay > 0 then
-        local storeTPModifier = 1 + actor:getMod(xi.mod.STORETP) / 100
-
-        return math.floor(xi.combat.tp.calculateTPReturn(actor, delay) * storeTPModifier)
+    if delay <= 0 then
+        return 0
     end
 
-    return 0
-end
+    local storeTPModifier = 1 + actor:getMod(xi.mod.STORETP) / 100
 
--- https://www.bg-wiki.com/ffxi/Tactical_Points
--- Gainee is the target who is going to gain the TP.
--- For instance, if a player attacks a mob, the mob uses the mob formula when gaining TP from the returned hit.
--- This appears to be a measure to not buff mobs when players were buffed with the new TP gain formula.
-xi.combat.tp.calculateTPReturn = function(gainee, delay)
-    local tpReturn = 0
-
-    if gainee and gainee:getObjType() ~= xi.objType.MOB then -- Pets and PCs have been observed to use this formula
-        if delay > 900 then
-            tpReturn = 173 + (delay - 900) * 28 / 360
-        elseif delay > 720 then
-            tpReturn = 161 + (delay - 720) * 24 / 360
-        elseif delay > 630 then
-            tpReturn = 154 + (delay - 630) * 28 / 360
-        elseif delay > 540 then
-            tpReturn = 149 + (delay - 540) * 20 / 360
-        elseif delay > 180 then
-            tpReturn = 61 + (delay - 180) * 88 / 360
-        else
-            tpReturn = 61 + (delay - 180) * 63 / 360
-        end
-    else -- mobs have been observed to use this formula -- http://wiki.ffo.jp/html/308.html
-        if delay > 530 then
-            tpReturn = 145 + (delay - 530) * 35 / 470
-        elseif delay > 480 then
-            tpReturn = 130 + (delay - 480) * 15 / 30
-        elseif delay > 450 then
-            tpReturn = 115 + (delay - 450) * 15 / 30
-        elseif delay > 180 then
-            tpReturn = 50 + (delay - 180) * 65 / 270
-        else
-            tpReturn = 50 + (delay - 180) * 15 / 180
-        end
-    end
-
-    return math.floor(tpReturn)
-end
-
--- Bonus subtle blow II from Tandem Blow (BST trait)
-xi.combat.tp.getTandemBlowBonus = function(actor)
-    local tandemBlowBonus = 0
-    if actor:isTandemActive() then
-        if actor:getMaster() ~= nil and actor:getMaster():isPC() then
-            tandemBlowBonus = actor:getMaster():getMod(xi.mod.TANDEM_BLOW_POWER)
-        else
-            tandemBlowBonus = actor:getMod(xi.mod.TANDEM_BLOW_POWER)
-        end
-    end
-
-    return tandemBlowBonus
+    return math.floor(xi.combat.tp.calculateTPReturn(actor, delay) * storeTPModifier)
 end
 
 -- TODO: does Ikishoten factor into this as a bonus to baseTPGain if it procs on the hit? Needs verification.
